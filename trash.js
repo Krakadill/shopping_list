601fd620cda42570decde7ca61ce404fac411d7d
luka orvelashvili <luke.orvela0@gmail.com>
2023-12-31 14:15:24 +0400

    first commit

---
+++ trash.js
@@ -0,0 +1,25 @@
+
+{
+// click events
+
+    /* const [name, setName] = useState("Luka");
+    const [count, setCount] = useState(0); */
+    /* const handleNameChange = () => {
+      const names = ["Luka", "Dave", "Josh"];
+      const int = Math.floor(Math.random() * 3);
+      setName(names[int]);
+    };
+  
+    const handleClick = () => {
+      setCount(count + 1);
+      console.log(count);
+    };
+  
+    const handleClick2 = (name) => {
+      console.log(count);
+    }; */
+    /*  <p onDoubleClick={handleClick}>Hello {name}!</p>
+        <button onClick={handleNameChange}>Change name it</button>
+        <button onClick={handleClick}>Click it</button>
+        <button onClick={handleClick2}>Click it</button> */
+  }
