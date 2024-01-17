149186259e05620b19d9ff68d1a7186f8f0a6a48
luka orvelashvili <luke.orvela0@gmail.com>
2024-01-17 15:24:04 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -4,125 +4,59 @@ import AddItem from "./AddItem";
 import Content from "./Content";
 import Footer from "./Footer";
 import { useState, useEffect } from "react";
-import apiRequest from "./apiRequest";
 
 function App() {
-  const API_URL = "http://localhost:3500/items";
-  const [items, setItems] = useState([]);
-
+  const [items, setItems] = useState(
+    JSON.parse(localStorage.getItem("shoppinglist")) || []
+  );
   const [newItem, setNewItem] = useState("");
   const [search, setSearch] = useState("");
-  const [fetchError, setFetchError] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    const fetchItems = async () => {
-      try {
-        const response = await fetch(API_URL);
-        if (!response.ok) throw Error("did not recieved data");
-        const listItems = await response.json();
-        setItems(listItems);
-        setFetchError(null);
-      } catch (err) {
-        console.log(err.message);
-        setFetchError(err.message);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-    setTimeout(() => {
-      fetchItems();
-    }, 3000);
-  }, []);
+    localStorage.setItem("shoppinglist", JSON.stringify(items));
+  }, [items]);
 
-  const setAndSaveItems = (newItems) => {
-    setItems(newItems);
-    localStorage.setItem("shopping list", JSON.stringify(newItems));
-  };
-
-  const addItem = async (item) => {
+  const addItem = (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
     const myNewItem = { id, checked: false, item };
     const listItems = [...items, myNewItem];
-    setAndSaveItems(listItems);
-    const postOptions = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(myNewItem),
-    };
-    const result = await apiRequest(API_URL, postOptions);
-    if (result) setFetchError(result);
+    setItems(listItems);
   };
 
-  const handleCheck = async (id) => {
+  const handleCheck = (id) => {
     const listItems = items.map((item) =>
       item.id === id ? { ...item, checked: !item.checked } : item
     );
     setItems(listItems);
-
-    const myItem = listItems.filter((item) => item.id === id);
-    const updateOptions = {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ checked: myItem[0].checked }),
-    };
-    const reqUrl = `${API_URL}/${id}`;
-    const result = await apiRequest(reqUrl, updateOptions);
-    if (result) setFetchError(result);
   };
 
-  const handleDelete = async (id) => {
+  const handleDelete = (id) => {
     const listItems = items.filter((item) => item.id !== id);
-    setAndSaveItems(listItems);
-
-    const deleteOptions = {
-      method: "DELETE",
-    };
-    const reqUrl = `${API_URL}/${id}`;
-    const result = await apiRequest(reqUrl, deleteOptions);
-    if (result) setFetchError(result);
+    setItems(listItems);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
     if (!newItem) return;
     addItem(newItem);
     setNewItem("");
   };
-
   return (
     <div className="App">
-      <Header title="Grocerie List" />
+      <Header title="Grocery List" />
       <AddItem
         newItem={newItem}
         setNewItem={setNewItem}
         handleSubmit={handleSubmit}
       />
       <SearchItem search={search} setSearch={setSearch} />
-      <main>
-        {isLoading && <p>Loading Items ...</p>}
-        {fetchError && (
-          <p
-            style={{
-              color: "red",
-            }}
-          >{`Error: ${fetchError}`}</p>
+      <Content
+        items={items.filter((item) =>
+          item.item.toLowerCase().includes(search.toLowerCase())
         )}
-        {!fetchError && !isLoading && (
-          <Content
-            items={items.filter((item) =>
-              item.item.toLowerCase().includes(search.toLocaleLowerCase())
-            )}
-            handleCheck={handleCheck}
-            handleDelete={handleDelete}
-          />
-        )}
-      </main>
+        handleCheck={handleCheck}
+        handleDelete={handleDelete}
+      />
       <Footer length={items.length} />
     </div>
   );
